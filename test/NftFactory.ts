706c34ff4d17a7b4fd713745c56107342fce16cd
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-16 18:12:11 +0700

    Add method to check only admin to set prefixBaseURI

--- test/NftFactory.ts
+++ test/NftFactory.ts
@@ -9,9 +9,10 @@ chai.use(solidity);
 context('#NftFactory', async() => {
     let nftContract: Contract;
     let admin: SignerWithAddress;
+    let account1: SignerWithAddress;
 
     beforeEach(async() => {
-        ([ admin ] = await ethers.getSigners()) 
+        ([ admin, account1 ] = await ethers.getSigners()) 
         const NftContract = await ethers.getContractFactory("NftFactory", admin);
         nftContract = await NftContract.deploy();
         await nftContract.deployed();
@@ -23,12 +24,16 @@ context('#NftFactory', async() => {
         await expect(ownerAddress).to.be.equal(admin.address);
     });
 
-    it('Set base url', async() => {
+    it('Set base uri', async() => {
         let baseURI = await nftContract.prefixBaseURI();
         expect(baseURI).to.be.eq("");
         const prefixBaseURI = "https://buni.nft";
         await nftContract.connect(admin).setPrefixBaseURI(prefixBaseURI);
         baseURI = await nftContract.prefixBaseURI();
         expect(baseURI).to.be.equal(prefixBaseURI);
-    })    
+    });    
+
+    it('Only set uri by admin', async() => {
+        await expect(nftContract.connect(account1).setPrefixBaseURI("TEMP")).to.be.revertedWith("Ownable: caller is not the owner");
+    })
 })
