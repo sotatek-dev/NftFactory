abfb097b331f5469fafe335244fc8b8cf252a936
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-16 16:22:11 +0700

    Add maping from deployedNft to NftInfo

--- contracts/NftFactory.sol
+++ contracts/NftFactory.sol
@@ -5,9 +5,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NftContract.sol";
 
 contract NftFactory is Ownable {
+    // TODO Add max number of deployed NFT for each user
+    // Mapping from user to own deployed NFTs
     mapping(address => address[]) private _deployedNfts; 
+    // Array of all deployed NFTs
     address[] private _allNfts;
 
+    struct NftInfo {
+        uint256 createdAt;
+        string name;
+        string symbol;
+        address deployedUser;
+    }
+    // Mapping from deployed NFT address to Nft info
+    mapping(address => NftInfo) private _nftInfos;
+
+    // prefixBaseURI to set for each deploy NFT, only set by owner of factory
     string public prefixBaseURI;
 
     event Deployed(address indexed deployer, address indexed deployedNft, uint256 indexed index);
@@ -30,6 +43,16 @@ contract NftFactory is Ownable {
         deployed = address(new NftContract(_name, _symbol, msg.sender, prefixBaseURI));
         _deployedNfts[msg.sender].push(deployed);
         _allNfts.push(deployed);
+        _nftInfos[deployed] = NftInfo(block.timestamp, _name, _symbol, msg.sender);
         emit Deployed(msg.sender, deployed, _allNfts.length - 1);
+    }
+
+    /**
+     * @notice Get deployed NFTs of an user
+     * @param  userAddress address of user
+     * @return deployedNfts Array of deployed NFTs of user
+     */
+    function getDeployedNfts(address userAddress) public view returns (address[] memory deployedNfts) {
+        return _deployedNfts[userAddress];
     }
 }
