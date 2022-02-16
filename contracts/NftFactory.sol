fcc8978a64f5ec7797205c79e3ba1e0e64e37389
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-16 15:28:30 +0700

    Add deploy method

---
+++ contracts/NftFactory.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./NftContract.sol";
+
+contract NftFactory is Ownable {
+    mapping(address => address[]) private _deployedNfts; 
+    address[] private _allNfts;
+
+    string public prefixBaseURI;
+
+    event Deployed(address indexed deployer, address indexed deployedNft, uint256 indexed index);
+
+    constructor() {
+        transferOwnership(msg.sender);
+    }
+
+    function deploy(string memory _name, string memory _symbol) external returns (address deployed) {
+        require(bytes(_name).length > 0, "Deploy::Name must not empty");
+        require(bytes(_symbol).length > 0, "Deploy::Symbol must not empty");
+        deployed = address(new NftContract(_name, _symbol, msg.sender, prefixBaseURI));
+        _deployedNfts[msg.sender].push(deployed);
+        _allNfts.push(deployed);
+        emit Deployed(msg.sender, deployed, _allNfts.length - 1);
+    }
+}
