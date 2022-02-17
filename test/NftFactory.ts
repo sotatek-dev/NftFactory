7c922bd2906c4afef84ba977d61ac0ed6f3bd704
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-17 09:57:32 +0700

    Refactor variable name and add stuff test for depoying contract

--- test/NftFactory.ts
+++ test/NftFactory.ts
@@ -7,33 +7,45 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 chai.use(solidity);
 
 context('#NftFactory', async() => {
-    let nftContract: Contract;
+    let nftFactory: Contract;
     let admin: SignerWithAddress;
     let account1: SignerWithAddress;
 
     beforeEach(async() => {
         ([ admin, account1 ] = await ethers.getSigners()) 
         const NftContract = await ethers.getContractFactory("NftFactory", admin);
-        nftContract = await NftContract.deploy();
-        await nftContract.deployed();
+        nftFactory = await NftContract.deploy();
+        await nftFactory.deployed();
     })
 
     it('Deploy success', async() => {
-        await expect(nftContract.address).to.be.properAddress;
-        const ownerAddress = await nftContract.owner();
+        await expect(nftFactory.address).to.be.properAddress;
+        const ownerAddress = await nftFactory.owner();
         await expect(ownerAddress).to.be.equal(admin.address);
     });
 
     it('Set base uri', async() => {
-        let baseURI = await nftContract.prefixBaseURI();
+        let baseURI = await nftFactory.prefixBaseURI();
         expect(baseURI).to.be.eq("");
         const prefixBaseURI = "https://buni.nft";
-        await nftContract.connect(admin).setPrefixBaseURI(prefixBaseURI);
-        baseURI = await nftContract.prefixBaseURI();
+        await nftFactory.connect(admin).setPrefixBaseURI(prefixBaseURI);
+        baseURI = await nftFactory.prefixBaseURI();
         expect(baseURI).to.be.equal(prefixBaseURI);
     });    
 
     it('Only set uri by admin', async() => {
-        await expect(nftContract.connect(account1).setPrefixBaseURI("TEMP")).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(nftFactory.connect(account1).setPrefixBaseURI("TEMP")).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    context('Deploy a NFT contract', async() => {
+        const contractName = "TestNFT";
+        const contractSymbol = "NFT";
+        beforeEach(async() => {
+            const tx = await nftFactory.connect(account1).deploy(contractName, contractSymbol);
+            console.log(tx);
+        });
+        it('Test stuff', async() => {
+            expect(1).to.be.eq(1);
+        });
     })
 })
