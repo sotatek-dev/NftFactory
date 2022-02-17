1403b2530f8f404c50ee76cd8d7ec4da0e99fafa
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-02-17 11:52:23 +0700

    Add NftContract test and fix bug concatenate string and address

--- contracts/NftContract.sol
+++ contracts/NftContract.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 contract NftContract is ERC721, Ownable {
     using Counters for Counters.Counter;
@@ -15,7 +16,7 @@ contract NftContract is ERC721, Ownable {
         ERC721(_name, _symbol)
     {
         transferOwnership(creator);
-        baseURI = string(abi.encodePacked(_prefixBaseURI, address(this), "/"));
+        baseURI = string(abi.encodePacked(_prefixBaseURI, _toAsciiString(address(this)), "/"));
     }
 
     /**
@@ -29,6 +30,23 @@ contract NftContract is ERC721, Ownable {
         _safeMint(to, _tokenIdCounter.current());
 
         return _tokenIdCounter.current();
+    }
+
+    function _toAsciiString(address x) private pure returns (string memory) {
+      bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = _char(hi);
+            s[2*i+1] = _char(lo);            
+        }
+        return string(abi.encodePacked(s));
+    }
+
+    function _char(bytes1 b) private pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
     }
 
     function _baseURI() internal view override returns (string memory) {
---
+++ test/NftContract.ts
@@ -0,0 +1,45 @@
+import chai, { expect } from "chai";
+import { Contract } from "ethers";
+import { solidity } from "ethereum-waffle";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+chai.use(solidity);
+
+context('#NftContract', async() => {
+    let nftFactory: Contract;
+    let nftContract: Contract;
+    let admin: SignerWithAddress;
+    let account1: SignerWithAddress;
+    const prefixBaseURI = "https://buni.nft/";
+    const contractName = "TestNFT";
+    const contractSymbol = "NFT";
+
+    beforeEach(async() => {
+        ([ admin, account1 ] = await ethers.getSigners());
+        const NftFactory = await ethers.getContractFactory("NftFactory", admin);
+        nftFactory = await NftFactory.deploy();
+        await nftFactory.deployed();
+
+        await nftFactory.connect(admin).setPrefixBaseURI(prefixBaseURI);
+        await nftFactory.connect(account1).deploy(contractName, contractSymbol);
+        const deployedAddress = await nftFactory.getNftOfUserByIndex(account1.address, 0);
+        nftContract = await ethers.getContractAt("NftContract", deployedAddress);
+    })
+    it('Test stuff', () => {
+        expect(1).to.be.equal(1);
+    });
+
+    it('Get correct name and symbol', async() => {
+        const name = await nftContract.name();
+        const symbol = await nftContract.symbol();
+        expect(name).to.be.equal(contractName);
+        expect(symbol).to.be.equal(contractSymbol);
+    });
+
+    it('Get correct URI', async() => {
+        const baseURI = await nftContract.baseURI();
+        const expectedURI = (`${prefixBaseURI}${nftContract.address.slice(2)}/`).toLowerCase();
+        expect(baseURI).to.be.equal(expectedURI);
+    });
+})
